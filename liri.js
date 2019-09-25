efdc76eb23fe42a605f578bc9de80aff9b68f0d4
Jandy <reverdy@protonmail.com>
2019-09-25 15:21:11 -0700

    pulled spotify-this-song function into it's own page

--- liri.js
+++ liri.js
@@ -11,47 +11,12 @@ var fs = require("fs");
 
 var Movie = require("./movie.js");
 var Concert = require("./concert.js");
+var Spotify = require("./spotify.js");
 
 var command = process.argv[2];
 // console.log(command);
 
 // var mediaQuery = process.argv[3].slice(2).join("+");
-
-function getSpotifySong() {
-  var song = process.argv.slice(3).join("+");
-  if (!song) {
-    song = "The Sign by Ace of Base";
-    // console.log(song);
-  }
-  //   console.log(song);
-
-  //   console.log(keys);
-
-  var spotify = new nodeSpotify({
-    id: keys.spotify.id,
-    secret: keys.spotify.secret
-  });
-
-  spotify
-    .search({ type: "track", query: song, limit: 10 })
-    .then(function(response) {
-      for (let i = 0; i < response.tracks.items.length; i++) {
-        // console.log(response.tracks.items[i].name);
-        var songData = {
-          songTitle: response.tracks.items[i].name,
-          album: response.tracks.items[i].album.name,
-          artist: response.tracks.items[i].artists[0].name,
-          previewLink: response.tracks.items[i].preview_url
-        };
-        console.log(songData);
-      }
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
-
-  // });
-}
 
 if (command === "movie-this") {
   // function(getMovieData);
@@ -61,7 +26,7 @@ if (command === "movie-this") {
   Concert();
 } else if (command === "spotify-this-song") {
   //   console.log("Spotify info here");
-  getSpotifySong();
+  Spotify();
 }
 
 // else if (command === "do-what-it-says") {
---
+++ spotify.js
@@ -0,0 +1,40 @@
+var nodeSpotify = require("node-spotify-api");
+var keys = require("./keys.js");
+
+function getSpotifySong() {
+  var song = process.argv.slice(3).join("+");
+  if (!song) {
+    song = "The Sign by Ace of Base";
+    // console.log(song);
+  }
+  //   console.log(song);
+
+  //   console.log(keys);
+
+  var spotify = new nodeSpotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+  });
+
+  spotify
+    .search({ type: "track", query: song, limit: 10 })
+    .then(function(response) {
+      for (let i = 0; i < response.tracks.items.length; i++) {
+        // console.log(response.tracks.items[i].name);
+        var songData = {
+          songTitle: response.tracks.items[i].name,
+          album: response.tracks.items[i].album.name,
+          artist: response.tracks.items[i].artists[0].name,
+          previewLink: response.tracks.items[i].preview_url
+        };
+        console.log(songData);
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+
+  // });
+}
+
+module.exports = getSpotifySong;
